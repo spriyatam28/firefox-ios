c3783dd5bc08f8334a82e389ef4c3c9d0cdf1dfb
PARAIPAN SORIN <51127880+PARAIPAN9@users.noreply.github.com>
2026-02-25 12:21:45 +0100

    Bugfix FXIOS-14961 - [Minimal toolbar] - Minimized Toolbar does not expand when tapped (#32251)
    * Add tap gesture recognizer for locationview.
    
    * Remove tap gesture recognizer.
    
    * Remove new line.
    
    * Remove location view tap gesture.

--- BrowserKit/Sources/ToolbarKit/AddressToolbar/LocationView/LocationView.swift
+++ BrowserKit/Sources/ToolbarKit/AddressToolbar/LocationView/LocationView.swift
@@ -449,7 +449,9 @@ final class LocationView: UIView,
                 let scaledTransformation = CGAffineTransform(scaleX: UX.smallScale, y: UX.smallScale)
                     .translatedBy(x: 0, y: yOffset)
                 self.transform = scaledTransformation
-                self.urlTextField.isUserInteractionEnabled = false
+            }, completion: { [unowned self] _ in
+                urlTextField.isUserInteractionEnabled = false
+                isUserInteractionEnabled = false
             })
     }
 
@@ -460,9 +462,9 @@ final class LocationView: UIView,
             options: [.curveEaseInOut],
             animations: { [unowned self] in
                 transform = .identity
-            },
-            completion: { [unowned self] _ in
+            }, completion: { [unowned self] _ in
                 urlTextField.isUserInteractionEnabled = true
+                isUserInteractionEnabled = true
             }
         )
     }
